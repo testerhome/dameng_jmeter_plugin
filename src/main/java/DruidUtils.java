e9095cf16e84dd4138c9d61d6fa33aa02948b1b7
jason <154274587@qq.com>
2023-11-17 19:28:02 +0800

    使用druid数据库连接池，优化了性能

--- src/main/java/DruidUtils.java
+++ src/main/java/DruidUtils.java
@@ -17,20 +17,15 @@ public class DruidUtils {
         try {
             //获取资源
             Properties ps = new Properties();
-            //打印当前路径是这个  /E:/apache-jmeter-5.4.1/bin/ApacheJMeter.jar!/org/apache/jmeter/
+
             File file = new File("");
             String execPath= file.getCanonicalPath();
             log.info(execPath);
-//            String realPath = execPath.substring(execPath.indexOf(":")+2, execPath.indexOf("!")-16);
-//            log.info(realPath);
+
             //加载资源
             // 使用InPutStream流读取properties文件 这种可以读取任意路径的
             BufferedReader bufferedReader = new BufferedReader(new FileReader(execPath+"\\config.properties"));
-//            InputStream fis = DruidUtils.class.getResourceAsStream("config.properties");
             ps.load(bufferedReader);
-
-            //只能读取resources文件夹下的
-//            ps.load(DruidUtils.class.getResourceAsStream("/config.properties"));
 
             //创建数据库连接池 利用配置文件
             dataSource = DruidDataSourceFactory.createDataSource(ps);
